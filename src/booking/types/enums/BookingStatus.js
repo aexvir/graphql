000a4fb9a95b0eb836f54b986ac84e4b15432719
Strajk <strajk@me.com>
2018-07-24 17:20:54 +0200

    Correct Booking statuses

--- src/booking/types/enums/BookingStatus.js
+++ src/booking/types/enums/BookingStatus.js
@@ -5,13 +5,9 @@ import { GraphQLEnumType } from 'graphql';
 export default new GraphQLEnumType({
   name: 'BookingStatus',
   values: {
-    NEW: { value: 'open' },
-    REFUNDED: { value: 'refunded' },
-    PENDING: { value: 'pending' },
-    CONFIRMED: { value: 'confirmed' },
-    CANCELLED: { value: 'cancelled' },
-    DELETED: { value: 'deleted' },
     CLOSED: { value: 'closed' },
-    EXPIRED: { value: 'expired' },
+    CONFIRMED: { value: 'confirmed' },
+    REFUNDED: { value: 'refunded' },
+    REFUNDING: { value: 'refunding' },
   },
 });
