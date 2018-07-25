d966279749d33ff5e7f3d7bd971419def0f0703e
Strajk <strajk@me.com>
2018-07-25 12:52:59 +0200

    Deprecate instead of removing

--- src/booking/types/enums/BookingStatus.js
+++ src/booking/types/enums/BookingStatus.js
@@ -2,12 +2,21 @@
 
 import { GraphQLEnumType } from 'graphql';
 
+const deprecationReason =
+  'Never valid value for Booking Status, only for Booking Extra Status';
+
 export default new GraphQLEnumType({
   name: 'BookingStatus',
   values: {
     CLOSED: { value: 'closed' },
     CONFIRMED: { value: 'confirmed' },
     REFUNDED: { value: 'refunded' },
-    REFUNDING: { value: 'refunding' },
+
+    // TODO: Remove deprecated after some deprecation period
+    NEW: { value: 'open', deprecationReason },
+    PENDING: { value: 'pending', deprecationReason },
+    CANCELLED: { value: 'cancelled', deprecationReason },
+    DELETED: { value: 'deleted', deprecationReason },
+    EXPIRED: { value: 'expired', deprecationReason },
   },
 });
