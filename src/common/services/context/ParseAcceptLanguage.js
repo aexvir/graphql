dfed63f1905a43524e5fec4c74a958aa655fe8dc
Dino Trojak <dinotrojak@gmail.com>
2018-08-10 15:35:09 +0200

    Added support for lovercase countries (#305)
    * added support for lovercase countries
    
    * added optional country

--- src/common/services/context/ParseAcceptLanguage.js
+++ src/common/services/context/ParseAcceptLanguage.js
@@ -2,12 +2,12 @@
 
 export default (acceptLanguage: ?string) => {
   let [language, territory] = ['en', 'US'];
-  // Valid acceptLanguage en_US || en-US || en-US,en;q=0.9,sl;q=0.8
-  const localeRegex = /([a-z][a-z])[-_]([A-Z][A-Z])/;
+  // Valid acceptLanguage en || en_us || en_US || en-US || en-US,en;q=0.9,sl;q=0.8
+  const localeRegex = /^([a-z]{2})(?:[-_]([A-Za-z]{2}))?(,|;|$)/;
 
   if (acceptLanguage && localeRegex.test(acceptLanguage)) {
-    [, language, territory] = localeRegex.exec(acceptLanguage);
+    [, language, territory = 'US'] = localeRegex.exec(acceptLanguage);
   }
 
-  return [language, territory];
+  return [language, territory.toUpperCase()];
 };
--- src/common/services/context/__tests__/ParseAcceptLanguage.test.js
+++ src/common/services/context/__tests__/ParseAcceptLanguage.test.js
@@ -9,12 +9,27 @@ describe('Parse Accept-Language', () => {
     expect(con).toBe('US');
   });
   it('should return default values for invalid lang format', () => {
+    const [lang, con] = parseAcceptLanguage('czech');
+    expect(lang).toBe('en');
+    expect(con).toBe('US');
+  });
+  it('should return default values for invalid lang format #2', () => {
     const [lang, con] = parseAcceptLanguage('cz+CZ');
     expect(lang).toBe('en');
     expect(con).toBe('US');
   });
   it('should return values for de_DE', () => {
     const [lang, con] = parseAcceptLanguage('de_DE');
+    expect(lang).toBe('de');
+    expect(con).toBe('DE');
+  });
+  it('should return values only for language', () => {
+    const [lang, con] = parseAcceptLanguage('de');
+    expect(lang).toBe('de');
+    expect(con).toBe('US');
+  });
+  it('should return values for lovercase country de_de', () => {
+    const [lang, con] = parseAcceptLanguage('de_de');
     expect(lang).toBe('de');
     expect(con).toBe('DE');
   });
